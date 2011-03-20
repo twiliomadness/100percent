486afd8e52074be939ba0290df878fb33e9affed
Steve Faulkner <southpolesteve@gmail.com>
2011-03-20 15:26:35 -0500

    Changed devise mappings to be more user friendly

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Gotv::Application.routes.draw do
   get "sms_messages/incoming"
 
-  devise_for :users
+  devise_for :users, :skip => [:registrations, :sessions] do
+    # devise/registrations
+    get 'signup'         => 'devise/registrations#new',     :as => :new_user_registration
+    post 'signup'         => 'devise/registrations#create', :as => :user_registration
+    get 'users/cancel'    => 'devise/registrations#cancel', :as => :cancel_user_registration
+    get 'users/edit'      => 'devise/registrations#edit',   :as => :edit_user_registration
+    put 'users'           => 'devise/registrations#update'
+    delete 'users/cancel' => 'devise/registrations#destroy'
+
+    # devise/sessions
+    get 'signin'  => 'devise/sessions#new',     :as => :new_user_session
+    post 'signin' => 'devise/sessions#create',  :as => :user_session
+    get 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   get "welcome/index"
   root :to => "welcome#index"
 end
+
