2deb97067e5dd228ccf24c31342dd8f5bb972d84
Philip Crawford <philip.crawford@uwalumni.com>
2011-03-20 16:19:44 -0500

    Update message for polling place

--- app/models/polling_place.rb
+++ app/models/polling_place.rb
@@ -8,6 +8,10 @@ class PollingPlace < ActiveRecord::Base
   # TODO: trim fields
   # http://www.mail-archive.com/rubyonrails-talk@googlegroups.com/msg22522.html
   
+  def sms_description
+    "#{self.location_name} #{self.address} #{self.city}"
+  end
+  
   def self.polling_place_id_from_link(polling_place_link)
     # Find this value:  PPLID=593214
     # https://vpa.wi.gov/PollingPlaceAccessibilityPage.aspx?Language=en-us&PPLID=593214&DistrictComboID=100012276&JurisdictionID=448
