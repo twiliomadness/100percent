99b7d0b765b4596413d3ea09007ebd1728804230
Philip Crawford <philip.crawford@uwalumni.com>
2011-03-20 21:11:33 -0500

    Clean up display of polling place info

--- app/models/polling_place.rb
+++ app/models/polling_place.rb
@@ -2,14 +2,17 @@ class PollingPlace < ActiveRecord::Base
   has_many :voters
   
   validates_presence_of :polling_place_id
+  validates_presence_of :location_name
+  validates_presence_of :address
+  validates_presence_of :city
   
-  before_save :clean_polling_place_name
+  before_save :clean_fields
   
   # TODO: trim fields
   # http://www.mail-archive.com/rubyonrails-talk@googlegroups.com/msg22522.html
   
   def sms_description
-    "#{self.location_name}, #{self.address}, #{self.city}"
+    "#{self.location_name}, #{self.address}, #{self.city}".titleize
   end
   
   def self.polling_place_id_from_link(polling_place_link)
@@ -18,9 +21,18 @@ class PollingPlace < ActiveRecord::Base
     rand(9999)
   end
   
+  def clean_fields
+    clean_polling_place_name
+    clean_address
+  end
+  
   def clean_polling_place_name
     # 058-04 MUNICIPAL BUILDING
     # 35 - WIL-MAR NEIGHBORHOOD CENTER
+  end
+  
+  def clean_address
+    self.address = self.address.gsub("  "," ").strip
   end
   
 end
