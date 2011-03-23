9effa19bfaeb4320ef90385c9fee3371997badc8
Stephen Anderson and Nick Karpenske <dev+bendycode+randland@bendyworks.com>
2011-03-23 18:47:43 -0500

    Add scaffolded admin/users views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,6 @@
-class Admin::UsersController < AdminController
+class Admin::UsersController < ApplicationController
+  layout 'admin'
+
+  expose(:user) { @user || User.find_by_id(params[:user_id] || params[:id]) || User.new }
+  expose(:users) { User.all.paginate(:per_page => 10, :page => params[:page]) }
 end
