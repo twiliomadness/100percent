401a8ded3602846e883e2cc5f60ee0a854161c67
Philip Crawford <philip.crawford@uwalumni.com>
2011-03-26 17:22:22 -0500

    scope the user list to those having email addresses.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,5 +2,5 @@ class Admin::UsersController < AdminController
   layout 'admin'
 
   expose(:user) { @user || User.find_by_id(params[:user_id] || params[:id]) || User.new }
-  expose(:users) { User.all.paginate(:per_page => 50, :page => params[:page]) }
+  expose(:users) { User.has_email.paginate(:per_page => 50, :page => params[:page]) }
 end
