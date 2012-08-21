7431cbd57c3f437abf8e451f83707a440ae32910
Philip Crawford <philipharry@gmail.com>
2012-08-20 21:56:45 -0500

    updating sending text for new gem

--- app/models/outgoing_message.rb
+++ app/models/outgoing_message.rb
@@ -7,19 +7,17 @@ class OutgoingMessage < TextMessage
     # If a message is two returns, it is "valid", but we shouldn't send it.
     if text.strip.length > 0 && self.voter.phone_number != SmsVoter::FAKE_PHONE_NUMBER
 
-      twilio = Twilio::REST::Client.new(APP_CONFIG[:TWILIO_ACCOUNT_SID], APP_CONFIG[:TWILIO_ACCOUNT_TOKEN])
+      @client = Twilio::REST::Client.new(APP_CONFIG[:TWILIO_ACCOUNT_SID], APP_CONFIG[:TWILIO_ACCOUNT_TOKEN])
       
       outgoing_number = self.voter.twilio_number_used.present? ? self.voter.twilio_number_used : APP_CONFIG[:TWILIO_CALLER_ID]
       
-      data = {
-        'From' => outgoing_number,
-        'To' => self.voter.phone_number,
-        'Body' => text
-      }
+      @client.account.sms.messages.create(
+        :from => outgoing_number,
+        :to => outgoing_number,
+        :body => text
+      )
 
-      path = "/2010-04-01/Accounts/#{APP_CONFIG[:TWILIO_ACCOUNT_SID]}/SMS/Messages"
-      twilio_response = twilio.request(path, 'POST', data)
-      twilio_response.error! unless twilio_response.kind_of? Net::HTTPSuccess
+      #twilio_response.error! unless twilio_response.kind_of? Net::HTTPSuccess
     end
   end
 
