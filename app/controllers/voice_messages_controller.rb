c85d8009c13a73c84e19323a9233eb394a4febf2
Philip Crawford <philipharry@gmail.com>
2012-08-22 08:37:23 -0500

    Dial caller into conf room - test

--- app/controllers/voice_messages_controller.rb
+++ app/controllers/voice_messages_controller.rb
@@ -1,20 +1,22 @@
 class VoiceMessagesController < ApplicationController
  # this is the handler for the initial phone call call and should correspond to the endpoint
  # configured in the Twilio dashboard for the app
- def incoming
-   # Limit the length of recordings
-   record_length = 360  # six minutes
-   phone_number = params[:Caller]
+  def incoming
+    # Limit the length of recordings
+    record_length = 360  # six minutes
+    phone_number = params[:Caller]
+    @response = Twilio::TwiML::Response.new do |r|
+      r.Say 'Hi, welcome to Vote Simple. Please hold while we try to connect you with a volunteer.', :voice => 'woman'
+      r.Dial do |d|
+        d.Conference 'myconfroom'
+      end
+    end
 
-   @response = Twilio::Response.new
-   @response.append(Twilio::Say.new("Hi, welcome to Vote Simple.  For help in voting and registering to vote, please leave name and we will call you back shortly.", :voice => "woman", :loop => "1"))
-   @response.append(Twilio::Record.new(:action => voice_messages_recording_url, :maxLength => record_length))
+    render :xml => @response.respond
 
-   render :xml => @response.respond
-
-   # do you want to log the fact that you’re doing this step?
-   # logging.info(FIXME - report that the caller called)
- end
+    # do you want to log the fact that you’re doing this step?
+    # logging.info(FIXME - report that the caller called)
+  end
 
  def recording
    phone_number = params[:Caller]
