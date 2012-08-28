2d8e6e3a86e52d1f2554151bbe9e01f0f665fbd8
Philip Crawford <philipharry@gmail.com>
2012-08-27 20:11:22 -0500

    more deleting

--- app/controllers/sms_messages_controller.rb
+++ app/controllers/sms_messages_controller.rb
@@ -11,37 +11,28 @@ class SmsMessagesController < ApplicationController
     twilio_number_used = params[:To]
 
     @user = User.find_or_create_by_phone_number(phone_number)
-    @user.sms_voter.update_attribute(:twilio_number_used, twilio_number_used) if @user.sms_voter.present?
-    @sms_voter = @user.sms_voter.nil? ? @user.create_sms_voter(:phone_number => phone_number, :twilio_number_used => twilio_number_used, :sms_city => sms_city, :sms_state => sms_state, :sms_zip => sms_zip) : @user.sms_voter
+    @user.voter.update_attribute(:twilio_number_used, twilio_number_used) if @user.voter.present?
+    @sms_voter = @user.voter.nil? ? @user.create_voter(:phone_number => phone_number, :twilio_number_used => twilio_number_used, :sms_city => sms_city, :sms_state => sms_state, :sms_zip => sms_zip) : @user.voter
     
     if @user.volunteer && ["on", "off"].include?(incoming_text.downcase)
       outgoing_text = @user.set_volunteer_status(incoming_text)
     else
-      outgoing_text = @sms_voter.process_message(incoming_text)
+      "Hello, for information on voting, please visit http://VoteSimple.org or call this number."
     end
 
-    if outgoing_text.kind_of?(String)
-      outgoing_text = [outgoing_text]
-    end
-
-    outgoing_text = outgoing_text.reject { |message| message.nil? || message.strip.blank? }
-
-    outgoing_text.each do |message|
-      m =  @sms_voter.outgoing_messages.create(:text => message)
-      sleep 1 if outgoing_text.size > 1
-    end
+    @voter.outgoing_messages.create(:text => outgoing_text) if outgoing_text.present?
 
     head 200
   end
 
-  private
+private
 
-    def validate_request
-      signature = request.headers['HTTP_X_TWILIO_SIGNATURE']
-      if Rails.env != "development" && !TwilioHelper.validateRequest(signature, request.url, request.post? ? params : {})
-        Exceptional.context(:environment => Rails.env)
-        raise "Invalid request with signature '#{signature}' for url '#{request.url}' with params: #{params}"
-      end
+  def validate_request
+    signature = request.headers['HTTP_X_TWILIO_SIGNATURE']
+    if Rails.env != "development" && !TwilioHelper.validateRequest(signature, request.url, request.post? ? params : {})
+      Exceptional.context(:environment => Rails.env)
+      raise "Invalid request with signature '#{signature}' for url '#{request.url}' with params: #{params}"
     end
+  end
 
 end
