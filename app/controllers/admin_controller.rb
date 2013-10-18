0b9a1c261797ba42afdb6fefcbdfdd01018766e2
Philip Crawford <philipharry@gmail.com>
2013-10-17 20:57:26 -0500

    missing controller?

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,15 @@
+class AdminController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :ensure_admin
+
+protected
+
+  def ensure_admin
+    if current_user.admin?
+
+    else
+      flash[:notice] = "You don't have rights to admin."
+      redirect_to root_path
+    end
+  end
+end
