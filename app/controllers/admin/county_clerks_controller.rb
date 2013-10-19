2ce451ef69d28b363062db34aade0f06f200167c
Mark McEahern <mark@mceahern.com>
2013-10-19 16:43:23 -0500

    Delete or comment out stuff not working

--- app/controllers/admin/county_clerks_controller.rb
+++ app/controllers/admin/county_clerks_controller.rb
@@ -1,4 +1,4 @@
-class Admin::CountyClerksController < AdminController
+class Admin::CountyClerksController < ApplicationController
   layout 'admin'
 
   expose(:county_clerk) { @county_clerk || CountyClerk.find_by_id(params[:county_clerk_id] || params[:id]) || CountyClerk.new }
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   def after_sign_in_path_for(user)
     if user.admin
       admin_path
